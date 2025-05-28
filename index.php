4daf3ac893d9c48e89121e70e3533e32afd67173
BuildTools <unconfigured@null.spigotmc.org>
2025-05-28 10:31:35 +0200

    index

--- index.php
+++ index.php
@@ -6,10 +6,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Simple Blog System (CMS)</title>
+    <link rel="stylesheet" href="./css/index.css">
 </head>
 <body>
 <nav>
-
+    <div class="logo">
+        <span>Nazwa</span>
+        <button class="login-button">ZALOGUJ</button>
+    </div>
+    <div class="nav-links">
+        <span>Strona główna</span>
+        <span>O mnie</span>
+        <span>Kontakt</span>
+    </div>
 </nav>
 <main>
 
