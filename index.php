dfb8810be1fe288ce680d9590addd1fabef18ebd
BuildTools <unconfigured@null.spigotmc.org>
2025-05-28 09:52:23 +0200

    1

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+?>
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Simple Blog System (CMS)</title>
+</head>
+<body>
+<nav>
+
+</nav>
+<main>
+
+</main>
+<footer>
+
+</footer>
+</body>
+</html>
