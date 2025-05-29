bfc6e6c9a2c23a7c55ef9757e5d598a3bc40f90b
BuildTools <unconfigured@null.spigotmc.org>
2025-05-29 14:38:47 +0200

    db + gitignore

--- inc/db.php
+++ inc/db.php
@@ -1,1 +1,9 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "SimpleBlogSystem";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
