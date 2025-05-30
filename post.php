1cedc9944eef8b70d2e2faf5d14c6391119a02b0
BuildTools <unconfigured@null.spigotmc.org>
2025-05-30 11:37:10 +0200

    post + comment

--- post.php
+++ post.php
@@ -3,25 +3,23 @@
 global $conn;
 include("./inc/db.php");
 
-if (!isset($_GET["id"]) || !is_numeric($_GET["id"])) {
-    echo "Error on _GET";
-    return;
-}
-$Postid = $_GET['id'];
-
-$sql = "SELECT title, content, created_at FROM posts WHERE id = ". $Postid;
-if ($stmt = mysqli_prepare($conn, $sql)) {
-    if (mysqli_stmt_execute($stmt)) {
-        $result = mysqli_stmt_get_result($stmt);
-        if (mysqli_num_rows($result) == 1) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                $title = $row["title"];
-                $content = $row["content"];
-                $created_at = $row["created_at"];
+if (isset($_GET["id"]) || is_numeric($_GET["id"])) {
+    $Postid = $_GET['id'];
+    $sql = "SELECT title, content, created_at FROM posts WHERE id = " . $Postid;
+    if ($stmt = mysqli_prepare($conn, $sql)) {
+        if (mysqli_stmt_execute($stmt)) {
+            $result = mysqli_stmt_get_result($stmt);
+            if (mysqli_num_rows($result) == 1) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $title = $row["title"];
+                    $content = $row["content"];
+                    $created_at = $row["created_at"];
+                }
             }
         }
     }
 }
+//if (isset($_POST['']))
 
 ?>
 <!doctype html>
@@ -30,7 +28,7 @@ if ($stmt = mysqli_prepare($conn, $sql)) {
     <meta name="author" content="Adrian Rzeszutek">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Simple Blog System (CMS)  | </title>
+    <title>Simple Blog System (CMS) | </title>
     <link rel="stylesheet" href="./css/index.css">
     <link rel="stylesheet" href="./css/post.css">
 </head>
@@ -48,16 +46,25 @@ if ($stmt = mysqli_prepare($conn, $sql)) {
 </nav>
 <main>
     <?php
-    echo '<h1>Czytasz aktualnie: '.$title.'</h1>';
+    echo '<h1>Czytasz aktualnie: ' . $title . '</h1>';
     echo '<div class="post-container">';
     echo '<div class="post-box">';
-    echo '<p>'.$content.'</p>';
+    echo '<p>' . $content . '</p>';
     echo '<button><a href="index.php">Powrót</a></button>';
-    echo '<span>'.$created_at.'</span>';
+    echo '<span>' . $created_at . '</span>';
     ?>
     </div></div>
     <h1>Sekcja komentarzy:</h1>
-
+    <div class="comments-container">
+        <form action="<?php echo htmlspecialchars('post.php'); ?>" method="post">
+            <div class="comment-box">
+                <h4>Skomentuj</h4>
+                <input class="author" name="author" maxlength="100" type="text" placeholder="Wpisz pseudonim">
+                <input class="comment" name="comment" type="text" placeholder="Twój komentarz">
+                <button type="submit">Dodaj</button>
+            </div>
+        </form>
+    </div>
 </main>
 <footer>
     <p>&copy; 2025 Adrian Rzeszutek</p>
