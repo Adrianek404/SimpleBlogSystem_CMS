2c3aca01f6ba2b6aa430757a89de5d6853dcfc2c
Adrianek404 <adr.rzeszutek@gmail.com>
2025-06-20 00:33:17 +0200

    Dodanie toast css i oraz fontawesome

--- post.php
+++ post.php
@@ -71,6 +71,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>Simple Blog System (CMS) | <?php echo $title; ?></title>
     <link rel="stylesheet" href="./css/index.css">
     <link rel="stylesheet" href="./css/post.css">
+    <link rel="stylesheet" href="./css/toast.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
 <nav>
